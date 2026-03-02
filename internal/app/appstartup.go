39fc35b02c73a338882deed7c330e159dcafd76e
john githiyon <johngithiyon7@gmail.com>
2026-03-02 23:02:02 +0530

    Write the func call for the docker conn obj

--- internal/app/appstartup.go
+++ internal/app/appstartup.go
@@ -1,8 +1,24 @@
 package app
 
-import "github.com/johngithiyon/Nodefy/internal/routes"
+import (
+	"log"
+
+	"github.com/johngithiyon/Nodefy/internal/repository/docker"
+	"github.com/johngithiyon/Nodefy/internal/routes"
+)
 
 func Appstartup() {
 	 
 	 routes.Routes()
+
+	 // docker connections 
+
+	 dockerconnerr :=  docker.DockerConn()
+
+	 if dockerconnerr != nil {
+		 log.Println("Docker Conn Error",dockerconnerr)
+		 return 
+	 }
+
+
 }
