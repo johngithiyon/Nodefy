f89cacd826393788846c77f3b3e769f267edbabe
john githiyon <johngithiyon7@gmail.com>
2026-03-06 06:29:51 +0530

    feat: add the func call for the connections

--- internal/app/appstartup.go
+++ internal/app/appstartup.go
@@ -3,13 +3,39 @@ package app
 import (
 	"log"
 
+	"github.com/johngithiyon/Nodefy/internal/config"
 	"github.com/johngithiyon/Nodefy/internal/repository/docker"
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/postgres"
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
 	"github.com/johngithiyon/Nodefy/internal/routes"
 )
 
 func Appstartup() {
+
+	//load the env file
+
+	 loaderr := config.Loadenv()
+
+	 if loaderr != nil {
+		 return 
+	 }
+
+	 //routes for the api 
 	 
 	 routes.Routes()
+
+	 //Create a connection object call
+
+	 connerr :=  postgres.GetPostgresConnection()
+
+	 if connerr != nil {
+           log.Println("Db conn err",connerr)
+		   return
+	 }
+
+	 // Create a  redis connection call 
+     
+	redis.RedisGetConnection()
 
 	 // docker connections 
 
