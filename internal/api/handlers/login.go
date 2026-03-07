79599c2f4fb4c5d6b5286bd8c28f9a5a1b24d089
john githiyon <johngithiyon7@gmail.com>
2026-03-07 17:26:28 +0530

    feat: add the login feature

--- internal/api/handlers/login.go
+++ internal/api/handlers/login.go
@@ -3,8 +3,12 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/johngithiyon/Nodefy/internal/models"
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/postgres"
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
+	"github.com/johngithiyon/Nodefy/internal/services"
 	"github.com/johngithiyon/Nodefy/pkg/response"
 )
 
@@ -20,8 +24,36 @@ func Loginhandler(w http.ResponseWriter, r *http.Request) {
 
 		  login.Username = r.FormValue("username")
 		  login.Password = r.FormValue("password")
+          
+		  passwd,passwderr := postgres.SearchPassword(login.Username)
 
-		  
+          if  passwd == "" && passwderr != nil {
+                response.Response(w,401,"Login Failed")
+				return 
+		  }
 
+		 comparerr :=  services.Comparepassword(login.Password,passwd)
+
+		 if comparerr != nil {
+			  response.Response(w,401,"Username or Password Mismatch")
+			  return 
+		 }
+
+		 id := services.GenerateSessionStore(login.Username)
+
+		 http.SetCookie(w,&http.Cookie{
+
+			 Name: "session-id",
+			 Value: id,
+			 Expires: time.Now().Add(7 * 24 * time.Hour),
+	 })
+
+	     seterr :=  redis.Storesessionid(id,login.Username)
+
+		 if seterr != nil {
+			  response.Response(w,500,"Internal Server Error")
+			  return 
+		 }
+		 
 		  response.Response(w,200,"Login Successfull")
 }
