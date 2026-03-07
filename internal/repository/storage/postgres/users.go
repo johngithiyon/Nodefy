9bbb6280bd5f620505642f938a66808387d7c48c
john githiyon <johngithiyon7@gmail.com>
2026-03-07 17:29:24 +0530

    feat: add the func for the pass search

--- internal/repository/storage/postgres/users.go
+++ internal/repository/storage/postgres/users.go
@@ -2,22 +2,51 @@ package postgres
 
 import (
 	"database/sql"
-
+	"log"
 
 	"github.com/johngithiyon/Nodefy/internal/models"
 )
 
 
-func  CheckUserexists(signup *models.Signup) (bool,error) {
+func  CheckUserexists(signup *models.Signup) bool {
 
-       _,usernamerr :=  Database.Db.Exec("select username from users where username = $1",signup.Username)
+	      var username string 
+
+        scanerr :=  Database.Db.QueryRow("select username from users where username = $1",signup.Username).Scan(&username)
        
-	   if usernamerr != nil && usernamerr == sql.ErrNoRows {   
-            return true,nil 
-		        
-	   } else {  
-		   return false,usernamerr
-	   }
-
+	    if scanerr != nil && scanerr == sql.ErrNoRows {
+			   return true
+		}  else {
+			return false 
+		}
 	   
+}
+
+func StoreUser(Verify *models.Verify) error {
+  
+  Res,inserterr := Database.Db.Exec("insert into users (username, email, password) VALUES ($1, $2, $3)",Verify.Username,Verify.Email,Verify.Password)
+
+  rowno, _ :=  Res.RowsAffected()
+
+  if rowno != 0 && inserterr != nil {
+	  log.Println("Insert Error for Users",inserterr)
+	  return inserterr
+  }
+	  
+  return nil 
+}
+
+func SearchPassword(username string) (string,error) {
+  
+	var passwd string 
+
+  searcherr := Database.Db.QueryRow("select password from users where username=$1",username).Scan(&passwd)
+
+  if searcherr != nil {
+	  log.Println("Search Err",searcherr)
+	  return "",searcherr
+  }
+	 
+	return  passwd,nil 
+	 
 }
