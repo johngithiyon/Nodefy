f9efc8804349f15a75da03d8498bd44f680e8a9c
john githiyon <johngithiyon7@gmail.com>
2026-03-08 10:29:11 +0530

    feat: add the stop container handler

---
+++ internal/api/handlers/stopcontainer.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
+	"github.com/johngithiyon/Nodefy/internal/services"
+	"github.com/johngithiyon/Nodefy/pkg/response"
+	"github.com/johngithiyon/Nodefy/pkg/utils"
+)
+
+func StopcontainerHandler(w http.ResponseWriter, r *http.Request) {
+
+	         //get the username to stop the container
+
+			sessionid,getsessionerr :=  utils.Getsessionid(r,"session-id")
+
+			if sessionid == "" && getsessionerr != nil {
+				 response.Response(w,400,"Cookie not found")
+				 return
+			}
+
+			username,getusernamerr := redis.Getusername(sessionid)
+
+			if username == "" && getusernamerr != nil {
+				  response.Response(w,500,"Internal Server Error")
+				  return
+			}
+
+           dockerstoperr :=  services.Stopcontainerservices(username)
+
+		   if dockerstoperr != nil {
+			   response.Response(w,500,"Internal Server Error")
+			   return
+		   }
+
+           response.Response(w,200,"Stop Instances Successfully")
+
+}
