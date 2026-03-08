e7e0041f70c213da5662997170be2b23ad9c2eda
john githiyon <johngithiyon7@gmail.com>
2026-03-08 11:11:17 +0530

    add the handler for kill container

---
+++ internal/api/handlers/killcontainer.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
+	"github.com/johngithiyon/Nodefy/internal/services"
+	"github.com/johngithiyon/Nodefy/pkg/response"
+	"github.com/johngithiyon/Nodefy/pkg/utils"
+)
+
+func Killcontainerhandler(w http.ResponseWriter, r *http.Request) {
+
+	       
+		if r.Method != http.MethodGet {
+			response.Response(w,405,"Invalid Metheod")
+			return 
+		}
+
+	    //get the username to stop the container
+
+		sessionid,getsessionerr :=  utils.Getsessionid(r,"session-id")
+
+		if sessionid == "" && getsessionerr != nil {
+			 response.Response(w,400,"Cookie not found")
+			 return
+		}
+
+		username,getusernamerr := redis.Getusername(sessionid)
+
+		if username == "" && getusernamerr != nil {
+			  response.Response(w,500,"Internal Server Error")
+			  return
+		}
+
+        killerr := services.Killcontainerservices(username)
+
+		if killerr != nil {
+			response.Response(w,500,"Internal Server Error")
+			return 
+		}
+
+		response.Response(w,200,"Kill Instance Successfully")
+
+}
