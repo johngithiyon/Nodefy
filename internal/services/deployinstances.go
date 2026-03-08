8dbc88e96e0aba0973ee908d61ea284ac3a11482
john githiyon <johngithiyon7@gmail.com>
2026-03-08 19:01:43 +0530

    refactor: make the proper func calls

--- internal/services/deployinstances.go
+++ internal/services/deployinstances.go
@@ -1,17 +1,49 @@
 package services
 
 import (
+	"log"
+
+	"github.com/johngithiyon/Nodefy/internal/errors"
 	"github.com/johngithiyon/Nodefy/internal/models"
 	"github.com/johngithiyon/Nodefy/internal/repository/docker"
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
 )
 
-func DeployInstances(username string,Deploy *models.Deploy ) error  {
+func DeployInstances(sessionid string,Deploy *models.Deploy ) error  {
+
+   //get username from redis 
+
+   username,geterr := redis.Getusername(sessionid)
+
+	if geterr != nil {
+		return errors.ErrInternalserver
+	}
+
+    // validate the user request
+
+	if Deploy.OsName != "ubuntu" && Deploy.OsName != "alphine" {
+			
+		log.Println("Invalid os")
+		return errors.ErrBadrequest
+	}
+
+	// validate the user selected services
+
+	for _,services := range Deploy.Services {
+	
+		if services != "postgresql" && services != "redis-server" {
+
+			log.Println("Invalid Services")
+			return errors.ErrBadrequest
+		}
+	}
+
 
    builerr :=  docker.BuildImage(username,Deploy)
 
    if builerr != nil {
-	     
-	     return builerr
+	     log.Println("Error in the build image",builerr)
+	     return errors.ErrInternalserver
    }
 
 	return nil 
