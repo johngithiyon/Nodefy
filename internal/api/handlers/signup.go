d6a1b9c826c4a7c588ba678a13a1607266874ee1
john githiyon <johngithiyon7@gmail.com>
2026-03-08 12:37:00 +0530

    refactor: imporve the architecture

--- internal/api/handlers/signup.go
+++ internal/api/handlers/signup.go
@@ -5,12 +5,10 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/johngithiyon/Nodefy/internal/errors"
 	"github.com/johngithiyon/Nodefy/internal/models"
-	"github.com/johngithiyon/Nodefy/internal/repository/storage/postgres"
-	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
 	"github.com/johngithiyon/Nodefy/internal/services"
 	"github.com/johngithiyon/Nodefy/pkg/response"
-	"github.com/johngithiyon/Nodefy/pkg/utils"
 )
 
 func Signuphandler(w http.ResponseWriter, r *http.Request) {
@@ -27,67 +25,17 @@ func Signuphandler(w http.ResponseWriter, r *http.Request) {
 		 Signup.Email = r.FormValue("email")
 		 Signup.Password = r.FormValue("password")
 
-		 //check the existence 
+		id,signuperr := services.Signupservices(&Signup)
 
-		    checkexists :=  postgres.CheckUserexists(&Signup)
-			 if !checkexists  {
-				  response.Response(w,409,"User Exists")
-				  return 
-			  }
-
-		 //valiadte username 
-
-		 if !(len(Signup.Username) <= 13)  {
-			log.Println("Username Length Issues")
-			response.Response(w,402,"Invalid Username")
-			return 		  
-		 }
-
-		 //validate email 
-
-		 emailcheck := utils.Regexcompile()
-
-		 if !emailcheck.MatchString(Signup.Email) {
-			log.Println("Email regex mismatch")
-			response.Response(w,402,"Invalid Email")
-			return 
-		 }
-
-		 //validate user password 
-         
-		 if !services.IsValidPassword(Signup.Password) {
-			    log.Println("Not a valid password")
-				response.Response(w,402,"Invalid Password")
-			    return 
-		 }
-
-		 //Hash the password 
-
-		 hashedpass,hasherr := services.Passwordhash(Signup.Password)
-
-		 if hasherr != nil {
-            log.Println("Hash Err",hasherr)
-			response.Response(w,500,"Internal Server Error")
-			return 
-		 }
-
-
-		otp := services.OtpGenerator(Signup.Email)
-
-		if otp == "" {
-			response.Response(w,500,"Internal Server Error")
+		if signuperr == errors.ErrAuthenticate {
+			response.Response(w,402,"Login Failed")
 			return 
 		}
 
-		senderr := services.SendEmail(Signup.Email,otp)
-
-		if senderr != nil {
-			log.Println("Mail Send Error",senderr)
+		if signuperr == errors.ErrInternalserver {
 			response.Response(w,500,"Internal Server Error")
-			return 
+			return
 		}
-
-		id := services.GenerateSessionStore(Signup.Username)
 		
 		//store the temp id in cookies 
 
@@ -98,20 +46,6 @@ func Signuphandler(w http.ResponseWriter, r *http.Request) {
 			 Expires: time.Now().Add(30 * time.Minute),
 	
 	 })
-
-		signupdetails := map[string]interface{} {
-			"username":Signup.Username,
-			"email":Signup.Email,
-			"password":hashedpass,
-			"otp":otp,
-		}  
-
-		storerr := redis.Storetemp(id,signupdetails)
-		
-        if  storerr != nil { 
-            response.Response(w,500,"Internal Server Error")
-			return
-	   } 
 
 		response.Response(w,200,"Signup Successful")
 
