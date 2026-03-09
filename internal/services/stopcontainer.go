358a60223b84603839dee3f258336e0a8fdd030a
john githiyon <johngithiyon7@gmail.com>
2026-03-10 04:10:51 +0530

    refactor: make the func calls properly

--- internal/services/stopcontainer.go
+++ internal/services/stopcontainer.go
@@ -1,8 +1,20 @@
 package services
 
-import "github.com/johngithiyon/Nodefy/internal/repository/docker"
+import (
+	"log"
 
-func Stopcontainerservices(username string) error{
+	"github.com/johngithiyon/Nodefy/internal/repository/docker"
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
+)
+
+func Stopcontainerservices(sessionid string) error{
+
+	username,getusernamerr := redis.Getusername(sessionid)
+
+	if username == "" && getusernamerr != nil {
+		  log.Println("Get username err from the stop container",getusernamerr)
+		  return getusernamerr
+	}
 
 	  dockerstoperr :=   docker.Stopcontainer(username)
 
