3a5a62d60b84b9a338f60d9da3507bc8ecc0ce6e
john githiyon <johngithiyon7@gmail.com>
2026-03-20 11:38:05 +0530

    feat: add the handlers for the frontend page

---
+++ internal/api/handlers/frontend.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/johngithiyon/Nodefy/internal/services"
+)
+
+func Rendersignupage(w http.ResponseWriter, r * http.Request) {
+          services.Render(w,"signup.html")
+}
+
+func Renderloginpage(w http.ResponseWriter, r *http.Request) {
+	     services.Render(w,"login.html")
+}
+
+func RenderOtpverifypage(w http.ResponseWriter, r *http.Request) {
+	 services.Render(w,"otpverification.html")
+}
+
+func Renderhomepage(w http.ResponseWriter,r *http.Request) {
+	services.Render(w,"index.html")
+}
+
+func Renderaboutpage(w http.ResponseWriter, r *http.Request) {
+	services.Render(w,"about.html")
+}
+
