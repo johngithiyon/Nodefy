109ad2714cc806e4eed44d7a820579a39db01549
john githiyon <johngithiyon7@gmail.com>
2026-03-20 14:28:24 +0530

    refactor : make the changes to build the instances

---
+++ internal/api/handlers/build.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/johngithiyon/Nodefy/internal/errors"
+	"github.com/johngithiyon/Nodefy/internal/models"
+	"github.com/johngithiyon/Nodefy/internal/services"
+
+	"github.com/johngithiyon/Nodefy/pkg/response"
+	"github.com/johngithiyon/Nodefy/pkg/utils"
+)
+
+func Buildhandler(w http.ResponseWriter, r *http.Request) {
+
+	var Build  models.Build
+
+    if r.Method != http.MethodPost {
+		 log.Println("Invalid Metheod")
+		 response.Response(w,405,"Invalid Metheod")
+		 return
+	}
+
+	//get the username to find the deploy user 
+ 
+	sessionid,getsessionerr :=  utils.Getsessionid(r,"session-id")
+
+	   if sessionid == "" && getsessionerr != nil {
+		     response.Response(w,400,"Cookie not found")
+			 return
+		}
+
+
+	// Convert the Json into Struct
+
+   decoderr := utils.Jstost(r.Body,&Build)
+
+   if decoderr != nil {
+	  log.Println("Decode Error",decoderr)
+	  response.Response(w,500,"Internal Server Error")
+	  return 
+   }
+
+    
+   builderr := services.BuildInstances(sessionid,&Build)
+
+   if builderr == errors.ErrInternalserver {
+	    response.Response(w,500,"Internal Server Error")
+		return
+   }
+
+   if builderr == errors.ErrBadrequest {
+	     response.Response(w,400,"Bad Request")
+		 return
+   }
+
+   response.Response(w,200,"Instances Deployed Successfully")
+
+}
