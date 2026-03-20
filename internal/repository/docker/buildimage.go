269cb05c94c39dea6f66a9b5c6ece9b32fa3c3f1
john githiyon <johngithiyon7@gmail.com>
2026-03-20 14:30:14 +0530

    refactor: make the major changes for the build

--- internal/repository/docker/buildimage.go
+++ internal/repository/docker/buildimage.go
@@ -5,81 +5,35 @@ import (
 	"os/exec"
 
 	"github.com/johngithiyon/Nodefy/internal/errors"
-	"github.com/johngithiyon/Nodefy/internal/models"
 	"github.com/johngithiyon/Nodefy/internal/repository/storage/postgres"
 	"github.com/johngithiyon/Nodefy/pkg/utils"
 )
 
-func BuildImage(username string,Deploy *models.Deploy) error {
 
-	        var service string 
-			
-            for _,services := range Deploy.Services {
-				 
-				   service += services + " "
-			}
+
+func BuildImage(username string,servicename string) error {
+	     
 
 			lowercase_username := utils.Lowercase(username)
 
-			//Write  a commmand to build the image
+            //pull and run the docker contiainer 
 
-			executer := exec.Command(
-				"docker", "build",
-				"--build-arg", "BASE_IMAGE="+Deploy.OsName,
-				"--build-arg", "PACKAGES="+service,
-				"-t", lowercase_username+"-"+Deploy.Instancename,
-				".",
+			cmd := exec.Command(
+				"docker", "run", "-d",
+				"--name",lowercase_username+"-"+servicename,
+				 servicename,
 			)
 
-			//Used to show the output and error from the cmd 
-		
-			output, outerr := executer.CombinedOutput()
-			if outerr != nil {
-				log.Println("Build failed:", outerr)
-				log.Println(string(output))
-				return outerr
+			output,outputerr := cmd.CombinedOutput()
+	
+			if outputerr != nil {
+                    log.Println("Failed in the services containers",outputerr)
+					return  outputerr
 			}
+   
+		   log.Println(string(output))
 
-		    log.Println(string(output))
-
-			// write the command to run the container
-
-			name := lowercase_username + "-" + Deploy.Instancename
-
-			runexecuter := exec.Command(
-				"docker", "run",
-
-				"--name", name,
-
-				"-u", "1000",
-				"-m", "100m",
-				"--memory-swap=100m",
-
-				"--network", "nodefy-network",
-
-				"--label", "owner="+username,
-				"--label", "instance="+Deploy.Instancename,
-
-				"-d",
-
-				
-				name,
-
-				"sleep",
-				"infinity",
-			)
-
-			runoutput,runerr := runexecuter.CombinedOutput()
-
-			if runerr != nil {
-				log.Println("Run failed:", runerr)
-				log.Println(string(runoutput))
-				return runerr
-			}
-
-		//save the instance name in db
-
-		  saverr :=  postgres.SaveInstance(Deploy.Instancename,username)
+		  saverr :=  postgres.SaveInstance(lowercase_username+"-"+servicename,username)
 
 		  if saverr != nil {
                 return errors.ErrInternalserver
