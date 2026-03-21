660961e98814523c760e992413b0054b574b7295
john githiyon <johngithiyon7@gmail.com>
2026-03-21 09:47:54 +0530

    feat: add the deploy handler

--- internal/api/handlers/deploy.go
+++ internal/api/handlers/deploy.go
@@ -1,60 +1,50 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/johngithiyon/Nodefy/internal/errors"
 	"github.com/johngithiyon/Nodefy/internal/models"
 	"github.com/johngithiyon/Nodefy/internal/services"
-
 	"github.com/johngithiyon/Nodefy/pkg/response"
 	"github.com/johngithiyon/Nodefy/pkg/utils"
 )
 
-func Deploy(w http.ResponseWriter, r *http.Request) {
+func DeployHandler(w http.ResponseWriter, r *http.Request) {
 
-	var Deploy models.Deploy
+	     var Deploy models.Deploy
 
-    if r.Method != http.MethodPost {
-		 log.Println("Invalid Metheod")
-		 response.Response(w,405,"Invalid Metheod")
-		 return
-	}
+		 if r.Method != http.MethodPost {
+			response.Response(w,405,"Invalid Metheod")
+			return
+	   }
+   
+	   //get the username to find the deploy user 
+	
+	   sessionid,getsessionerr :=  utils.Getsessionid(r,"session-id")
+   
+		  if sessionid == "" && getsessionerr != nil {
+				response.Response(w,400,"Cookie not found")
+				return
+		   }
+   
+   
+	   // Convert the Json into Struct
+   
+	  decoderr := utils.Jstost(r.Body,&Deploy)
+   
+	  if decoderr != nil {
+		 response.Response(w,500,"Internal Server Error")
+		 return 
+	  }
 
-	//get the username to find the deploy user 
- 
-	sessionid,getsessionerr :=  utils.Getsessionid(r,"session-id")
+	  deployerr := services.Deployservices(sessionid,&Deploy)
 
-	   if sessionid == "" && getsessionerr != nil {
-		     response.Response(w,400,"Cookie not found")
-			 return
-		}
+      if deployerr != nil {
+		   response.Response(w,500,"Internal Server Error")
+		   return
+	  }
 
+	  response.Response(w,200,"Project Deployed Successfully")
 
-	// Convert the Json into Struct
-
-   decoderr := utils.Jstost(r.Body,&Deploy)
-
-   if decoderr != nil {
-	  log.Println("Decode Error",decoderr)
-	  response.Response(w,500,"Internal Server Error")
-	  return 
-   }
-
-    
-   deployerr := services.DeployInstances(sessionid,&Deploy)
-
-   if deployerr == errors.ErrInternalserver {
-	    response.Response(w,500,"Internal Server Error")
-		return
-   }
-
-   if deployerr == errors.ErrBadrequest {
-	     response.Response(w,400,"Bad Request")
-		 return
-   }
-
-   response.Response(w,200,"Instances Deployed Successfully")
-
+   
 }
