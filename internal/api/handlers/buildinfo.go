5e88cf28b7f4595302f7c9406f72966e5fa7876b
john githiyon <johngithiyon7@gmail.com>
2026-03-24 21:43:52 +0530

    refactor: rename the file name into buildinfo

---
+++ internal/api/handlers/buildinfo.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/johngithiyon/Nodefy/internal/errors"
+	"github.com/johngithiyon/Nodefy/internal/services"
+	"github.com/johngithiyon/Nodefy/pkg/response"
+	"github.com/johngithiyon/Nodefy/pkg/utils"
+)
+
+func GetBuildInstancesHandler(w http.ResponseWriter,r *http.Request) {
+
+	     if r.Method != http.MethodGet {
+			  response.Response(w,405,"Invalid Method Type")
+			  return 
+		 }
+        
+	    
+	     sessionid,sessionerr := utils.Getsessionid(r,"session-id")
+
+		 if sessionerr != nil {
+			  response.Response(w,400,errors.ErrBadrequest.Error())
+              return 
+         }
+
+         instances,getinstancerr := services.Getinstanceservices(sessionid)
+
+		 if getinstancerr != nil {
+			return
+		 }
+
+		w.Header().Set("Content-Type", "application/json")
+        w.Write([]byte(instances))
+}		 
