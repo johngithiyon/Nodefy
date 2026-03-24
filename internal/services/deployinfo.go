ef829aff0fc716ff5232894779f8562327fc03f1
john githiyon <johngithiyon7@gmail.com>
2026-03-24 21:56:07 +0530

    feat: add the services for the deploy info

---
+++ internal/services/deployinfo.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"github.com/johngithiyon/Nodefy/internal/errors"
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/postgres"
+	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
+	"github.com/johngithiyon/Nodefy/pkg/utils"
+)
+
+func Getdeployinfo(sessionid string) (string,error)  {
+ 
+	username,getusernamerr := redis.Getusername(sessionid)
+ 
+	if getusernamerr != nil {
+		return "",errors.ErrInternalserver
+	}
+
+	deployinstances,geterr := postgres.Getdeployinstance(username)
+
+	if geterr != nil {
+		 return "",errors.ErrInternalserver
+	}
+
+	resp,converr := utils.Jsonconvertor(deployinstances)
+
+	if converr != nil {
+		return "",errors.ErrInternalserver
+	}
+
+	return resp,nil 
+}
