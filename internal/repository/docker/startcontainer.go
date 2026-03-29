8aca8fbb604dcc702c563c01465a03883ab1d514
john githiyon <johngithiyon7@gmail.com>
2026-03-29 11:21:59 +0530

    feat: write the func for the start container

---
+++ internal/repository/docker/startcontainer.go
@@ -0,0 +1,37 @@
+package docker
+
+import (
+	"log"
+	"os/exec"
+
+	"github.com/johngithiyon/Nodefy/internal/models"
+)
+
+func Startcontainer(username string,startcontainer models.Startcontainer) error {
+
+		cmd := exec.Command(
+			"docker",
+			"ps",
+			"-aq",
+			"--filter", "label=owner="+username,
+			"--filter","label=instance="+startcontainer.Instancename,
+		)
+
+	   output,outputerr :=  cmd.CombinedOutput()
+
+		if outputerr != nil {
+			log.Println("Docker Ps  Error in start container ",outputerr,string(output))
+			return outputerr
+		}
+
+		startcmd := exec.Command("docker","start",string(output))
+
+	    startoutput,starterr := startcmd.CombinedOutput()
+
+		if starterr != nil {
+			log.Println("Start Error",starterr,string(startoutput))
+			return starterr
+		}
+
+	return nil
+}
