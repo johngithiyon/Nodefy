61559db6d9e37397d94f40088e68cd3e9d027367
john githiyon <johngithiyon7@gmail.com>
2026-03-29 14:03:02 +0530

    feat: add the func to remove the instances info

---
+++ internal/repository/storage/postgres/killcontainer.go
@@ -0,0 +1,46 @@
+package postgres
+
+import (
+	"log"
+
+	"github.com/johngithiyon/Nodefy/internal/models"
+)
+
+func Containerkill(username string, Containerkill models.Containermanage) error {
+
+	if Containerkill.Type == "build" {
+
+		userid, getiderr := Getuserid(username)
+		if getiderr != nil {
+			return getiderr
+		}
+
+		query := `
+		DELETE FROM instances
+		WHERE user_id = $1
+		AND instance_name = $2;
+		`
+
+		_, deleteErr := Database.Db.Exec(query, userid, Containerkill.Instancename)
+		if deleteErr != nil {
+			return deleteErr
+		}
+	}
+
+	if Containerkill.Type == "deploy" {
+
+		query := `
+		DELETE FROM deploy_instances_services
+		WHERE app_id = $1
+		AND service_name = $2;
+		`
+
+		_, deleteErr := Database.Db.Exec(query, 9, Containerkill.Instancename)
+		if deleteErr != nil {
+			log.Println("Error deleting in deploy_instances_services:", deleteErr)
+			return deleteErr
+		}
+	}
+
+	return nil
+}
