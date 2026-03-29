1bd90073027e40d87ffa3361583be96d5ce42c92
john githiyon <johngithiyon7@gmail.com>
2026-03-29 09:26:36 +0530

    feat: add the func for the builaddinstances

---
+++ internal/repository/docker/buildaddinstances.go
@@ -0,0 +1,24 @@
+package docker
+
+import (
+	"log"
+	"os/exec"
+
+	"github.com/johngithiyon/Nodefy/internal/models"
+)
+
+func Addinstances(username string,Addinstances models.Addinstancesbuild) error {
+
+	// start the container by the user given image name 
+	
+	  cmd :=  exec.Command("docker","run","--name",Addinstances.Instancename,"-d",Addinstances.Imagename)
+
+      output,outputerr :=  cmd.CombinedOutput()
+
+	  if outputerr != nil {
+		  log.Println("Outputerr in the Add Instances",outputerr,string(output))
+		  return  outputerr
+	  }
+
+	   return  nil 
+}
