ad03fd5a4f5bd5ae25e99a116530d32c65a768f2
john githiyon <johngithiyon7@gmail.com>
2026-03-29 11:16:46 +0530

    feat: add the start container handler

---
+++ internal/api/handlers/startcontainer.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/johngithiyon/Nodefy/internal/models"
+	"github.com/johngithiyon/Nodefy/internal/services"
+	"github.com/johngithiyon/Nodefy/pkg/response"
+	"github.com/johngithiyon/Nodefy/pkg/utils"
+)
+
+func Startcontainerhandler(w http.ResponseWriter, r *http.Request) {
+
+	     var startcontainer models.Startcontainer
+
+		 if r.Method != http.MethodPost {
+			response.Response(w,405,"Invalid Metheod")
+			return
+	   }
+   
+	   //get the username to find the deploy user 
+	
+	   sessionid,getsessionerr :=  utils.Getsessionid(r,"session-id")
+   
+		  if sessionid == "" && getsessionerr != nil {
+				response.Response(w,400,"Cookie not found")
+				return
+		   }
+   
+   
+	   // Convert the Json into Struct
+   
+	  decoderr := utils.Jstost(r.Body,&startcontainer)
+   
+	  if decoderr != nil {
+		 response.Response(w,500,"Internal Server Error")
+		 return 
+	  }
+
+	 starterr :=  services.Startcontainerserives(sessionid,&startcontainer)
+
+	 if starterr != nil {
+		response.Response(w,500,"Internal Server Error")
+		return 
+	 }
+
+	 response.Response(w,200,"Start Container Sucessfully")
+}
