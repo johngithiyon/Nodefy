513322451bf41cf41776bdaf3448fad2dad3dc35
john githiyon <johngithiyon7@gmail.com>
2026-03-30 13:47:20 +0530

    feat: add the user to the container

--- internal/services/exploreos.go
+++ internal/services/exploreos.go
@@ -4,6 +4,7 @@ import (
 	"github.com/johngithiyon/Nodefy/internal/errors"
 	"github.com/johngithiyon/Nodefy/internal/models"
 	"github.com/johngithiyon/Nodefy/internal/repository/docker"
+	"github.com/johngithiyon/Nodefy/internal/repository/linux"
 	"github.com/johngithiyon/Nodefy/internal/repository/storage/redis"
 	"github.com/johngithiyon/Nodefy/pkg/utils"
 )
@@ -25,6 +26,19 @@ func Exploreos(sessionid string,exploreos *models.Exploreos) error {
 
   if explorerr != nil {
 	 return  errors.ErrInternalserver
+  }
+
+  userexists := linux.CheckUserlinux(username)
+
+   if userexists != nil {
+
+   adduserr := linux.Adduser(username)
+
+   if adduserr != nil {
+	   return adduserr
+   }
+
+	return nil 
   }
 	 
 	   return  nil 
