637fff6a5034c03c800eb4da9c2aba54a7027948
john githiyon <johngithiyon7@gmail.com>
2026-07-14 10:27:04 +0530

    feat: write the handler to decide the req flow to the workspace handler

---
+++ internal/api/handlers/roothandler.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/johngithiyon/Nodefy/pkg/utils"
+)
+
+func Roothandler(w http.ResponseWriter,r *http.Request) {
+	      
+	      host := r.Host
+
+		  check := "workspace.nodefy.in"
+
+		  checker := utils.Istringcheck(host,check)
+
+		  if checker {
+			      
+			    WorkspaceHanlder(w,r)
+			    
+		  }
+		   
+		  Renderhomepage(w,r)
+		  
+}
